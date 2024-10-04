17a9dc0522dc26a7d1abb74e9795abbb5c21f80f
Leo-Paul Geneau <leo-paul.geneau@nexedi.com>
2024-10-04 19:03:38 +0900

    setup.py: fix version naming

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     keywords="PIM-DM Multicast Routing Protocol PIM Dense-Mode Router RFC3973 IPv4 IPv6",
-    version="1.4.0nxd002",
+    version="1.4.0+nxd002",
     url="http://github.com/pedrofran12/pim_dm",
     author="Pedro Oliveira",
     author_email="pedro.francisco.oliveira@tecnico.ulisboa.pt",
